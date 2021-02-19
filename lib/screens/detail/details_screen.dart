e492f28ba2be65414dabf3f4a2e8b3835174b249
David Castello <davidcastellobatetgmail.com>
2021-02-19 14:36:58 +0100

    body > product title with image

--- lib/screens/detail/details_screen.dart
+++ lib/screens/detail/details_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:online_shop_ui_app_flutter/constants.dart';
 import 'package:online_shop_ui_app_flutter/models/Product.dart';
+import 'package:online_shop_ui_app_flutter/screens/detail/components/body.dart';
 
 class DetailsScreen extends StatelessWidget {
   final Product product;
@@ -9,7 +12,38 @@ class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Container(),
+      child: Scaffold(
+        backgroundColor: product.color,
+        appBar: buildAppBar(context),
+        body: Body(
+          product: product,
+        ),
+      ),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: product.color,
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset(
+          'assets/icons/back.svg',
+          color: Colors.white,
+        ),
+        onPressed: () => Navigator.pop(context),
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: SvgPicture.asset('assets/icons/search.svg'),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: SvgPicture.asset('assets/icons/cart.svg'),
+          onPressed: () {},
+        ),
+        SizedBox(width: kDefaultPadding / 2),
+      ],
     );
   }
 }
